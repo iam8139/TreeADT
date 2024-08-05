5c7aa68a1c93e061db8b3ea5893db65f3edb8d46
kash <iam8139@gmail.com>
2024-08-05 18:12:16 +0530

    LinkedTree implementation

--- src/main/java/org/game/serviceImpl/LinkedBinaryTree.java
+++ src/main/java/org/game/serviceImpl/LinkedBinaryTree.java
@@ -2,9 +2,11 @@ package org.game.serviceImpl;
 
 import org.game.dto.Position;
 
+import java.awt.*;
 import java.util.Iterator;
+import java.util.LinkedList;
 
-public class LinkedBinaryTree<T> extends AbstractBinaryTree<T>{
+public class LinkedBinaryTree<T> extends AbstractBinaryTree<T> {
     protected static class Node<T> implements Position<T> {
         private Node<T> parent;
         private Node<T> left;
@@ -90,5 +92,78 @@ public class LinkedBinaryTree<T> extends AbstractBinaryTree<T>{
     @Override
     public Iterable<Position<T>> positions() {
         return null;
+    }
+
+    public Position<T> addRoot(T p) {
+        if (!isEmpty()) throw new IllegalStateException("Tree is not Empty");
+        root = createNode(null, null, null, p);
+        size = 1;
+        return root;
+    }
+
+    public Position<T> addLeft(Position<T> p, T e) {
+        Node<T> node = validate(p);
+        if (node.getLeft() != null) throw new IllegalStateException("Position p already has a left child");
+        node.setLeft(createNode(node, null, null, e));
+        size++;
+        return node.getLeft();
+    }
+
+    public Position<T> addRight(Position<T> p, T e) {
+        Node<T> node = validate(p);
+        if (node.getRight() != null) throw new IllegalStateException("Position p already has a right child");
+        node.setRight(createNode(node, null, null, e));
+        size++;
+        return node.getRight();
+    }
+
+    public T set(Position<T> p, T e) {
+        Node<T> node = validate(p);
+        T temp = node.getElement();
+        node.setElement(e);
+        return temp;
+    }
+
+    public void attach(Position<T> p, LinkedBinaryTree<T> left, LinkedBinaryTree<T> right) {
+        Node<T> node = validate(p);
+        if (isInternal(node)) throw new IllegalStateException("Node must be a leaf");
+
+        if (!left.isEmpty()) {
+            left.root.setParent(node);
+            node.setLeft(left.root);
+            left.root = null;
+            left.size = 0;
+        }
+
+        if (!right.isEmpty()) {
+            right.root.setParent(node);
+            node.setRight(right.root);
+            right.root = null;
+            right.size = 0;
+        }
+
+        size += left.size() + right.size();
+    }
+
+    public T remove(Position<T> p) {
+        Node<T> node = validate(p);
+        if (numChildren(p) == 2) throw new IllegalArgumentException("Node has two children");
+        Node<T> child = node.getLeft() != null ? node.getLeft() : node.getRight();
+        if (child != null) child.setParent(node.getParent());
+        if (node == root) {
+            root = child;
+        } else {
+            Node<T> parent = node.getParent();
+            if (node == parent.getLeft()) parent.setLeft(child);
+            else parent.setRight(child);
+        }
+        size--;
+        T temp = node.getElement();
+        node.setRight(null);
+        node.setLeft(null);
+        node.setElement(null);
+        node.setParent(node);
+
+        return temp;
     }
 }
